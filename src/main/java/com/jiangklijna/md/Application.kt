69e371a8f66dd11a220215966e140c3b506197da
jiangKlijna <leil729564090@outlook.com>
2018-03-22 20:38:29 +0800

    :boom:init project

---
+++ src/main/java/com/jiangklijna/md/Application.kt
@@ -0,0 +1,12 @@
+package com.jiangklijna.md
+
+class Application {
+
+	companion object {
+
+		@JvmStatic
+		fun main(args: Array<String>) {
+			print("hello md")
+		}
+	}
+}
---
+++ src/test/java/com/jiangklijna/md/HttpTest.kt
@@ -0,0 +1,11 @@
+package com.jiangklijna.md
+
+import org.junit.Test
+
+class HttpTest {
+
+	@Test
+	fun request() {
+	}
+
+}
