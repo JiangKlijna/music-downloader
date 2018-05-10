5ef4a23166e105090b670ef32c5968718dd8c699
jiangKlijna <leil729564090@outlook.com>
2018-05-10 19:08:13 +0800

    :fire:add MusicPlatForm

---
+++ src/main/java/com/jiangklijna/md/bean/MusicPlatForm.kt
@@ -0,0 +1,11 @@
+package com.jiangklijna.md.bean
+
+enum class MusicPlatForm(val title: String) {
+	wy("网易音乐"),
+	xm("虾米音乐"),
+	qq("qq音乐"),
+	bd("百度音乐"),
+	kg("酷狗音乐");
+
+	val path: String = "img/$name.png"
+}
