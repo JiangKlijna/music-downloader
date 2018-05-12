6d469074bebc3b738ddaaf82132d55d69da5d51c
jiangKlijna <leil729564090@outlook.com>
2018-05-12 18:11:18 +0800

    :fire:musicview layout

--- src/main/java/com/jiangklijna/md/view/MusicView.kt
+++ src/main/java/com/jiangklijna/md/view/MusicView.kt
@@ -1,6 +1,15 @@
 package com.jiangklijna.md.view
 
+import com.jiangklijna.md.bean.MusicPlatForm
 import javafx.scene.layout.VBox
 
 class MusicView : VBox() {
+
+	init {
+		MusicPlatForm::class.java.enumConstants.map {
+			LogoView(it)
+		}.run {
+			children.addAll(this)
+		}
+	}
 }
