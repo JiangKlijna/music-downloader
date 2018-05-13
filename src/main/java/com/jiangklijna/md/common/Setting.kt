ac721b0057ef798f367260542b73ccf10084d045
jiangKlijna <leil729564090@outlook.com>
2018-05-13 17:15:11 +0800

    :fire:add Setting.kt

---
+++ src/main/java/com/jiangklijna/md/common/Setting.kt
@@ -0,0 +1,10 @@
+package com.jiangklijna.md.common
+
+object Setting {
+
+	const val width = 800.0
+	const val height = 800.0
+
+	const val LOGO_PATH = "/img/logo.png"
+	const val title = "Music Download"
+}
