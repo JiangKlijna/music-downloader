3b1d8ad497f03151283f94780c74b8a5c6bc8947
jiangKlijna <leil729564090@outlook.com>
2018-05-27 18:27:32 +0800

    :fire:update Application

--- src/main/java/com/jiangklijna/md/Application.kt
+++ src/main/java/com/jiangklijna/md/Application.kt
@@ -1,34 +1,23 @@
 package com.jiangklijna.md
 
-import com.jiangklijna.md.common.Setting
-import com.jiangklijna.md.view.MainView
-import javafx.scene.image.Image
-import javafx.stage.Stage
+import com.jiangklijna.md.app.CmdApp
+import com.jiangklijna.md.app.FxApp
+import com.jiangklijna.md.common.Function.ifnot
+import com.jiangklijna.md.common.Function.iftrue
+import com.jiangklijna.md.common.Function.println
 
-typealias FxApp = javafx.application.Application
+object Application {
 
-class Application : FxApp() {
+	val types = arrayListOf("fx", "cmd")
+	val default_type = types.first()
 
-	override fun start(primaryStage: Stage) {
-		primaryStage.title = Setting.title
-		primaryStage.minWidth = Setting.width
-		primaryStage.maxWidth = Setting.width
-		primaryStage.minHeight = Setting.height
-		primaryStage.maxHeight = Setting.height
-		primaryStage.isResizable = Setting.isResizable
-		primaryStage.icons.add(Image(Setting.LOGO_PATH))
-		primaryStage.scene = MainView.getScene()
-		primaryStage.show()
-	}
+	@JvmStatic
+	fun main(args: Array<String>) {
+		val type = if (args.isEmpty()) default_type else args.first()
 
-	override fun stop() {
-		super.stop()
-	}
-
-	companion object {
-
-		@JvmStatic
-		fun main(args: Array<String>) =
-				FxApp.launch(Application::class.java, *args)
+		var tag = false
+		type.equals(types.first(), true).iftrue { FxApp.run() }.run { tag = tag || this }
+		type.equals(types.last(), true).iftrue { CmdApp.run() }.run { tag = tag || this }
+		tag.ifnot { "command in $types".println() }
 	}
 }
