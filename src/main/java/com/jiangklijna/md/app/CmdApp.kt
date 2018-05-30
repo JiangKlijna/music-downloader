8e142f3704284d426c543a7ecaa80107213d2072
jiangKlijna <leil729564090@outlook.com>
2018-05-30 18:31:30 +0800

    :fire:start

--- src/main/java/com/jiangklijna/md/app/CmdApp.kt
+++ src/main/java/com/jiangklijna/md/app/CmdApp.kt
@@ -1,10 +1,23 @@
 package com.jiangklijna.md.app
 
+import com.jiangklijna.md.common.Logic.search
+import com.jiangklijna.md.bean.MusicPlatform
+import java.util.*
+
 class CmdApp {
 
+	var level = 0
+	var platform: MusicPlatform = MusicPlatform.wy
+
+	val read = Scanner(System.`in`)
+
 	fun start() {
-		println("hello")
+		println(platform.title + "搜索:")
+		val key = read.next()
+		val list = platform.search(key)
+		list.forEachIndexed { index, music -> println(music) }
 	}
+
 
 	companion object {
 		val run = { CmdApp().start() }
