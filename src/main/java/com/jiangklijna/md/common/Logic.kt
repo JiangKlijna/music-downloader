89cf1bbdd8cdcaf04ebc9b536d53adca50378406
jiangKlijna <leil729564090@outlook.com>
2018-06-11 18:01:52 +0800

    :fire:update Logic

--- src/main/java/com/jiangklijna/md/common/Logic.kt
+++ src/main/java/com/jiangklijna/md/common/Logic.kt
@@ -8,16 +8,45 @@ import java.io.File
 object Logic {
 
 
-	inline fun MusicPlatform.search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
+	fun MusicPlatform.search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true)
+			= when (this) {
+		MusicPlatform.wy -> this@Logic::wy_search
+		MusicPlatform.xm -> this@Logic::xm_search
+		MusicPlatform.qq -> this@Logic::qq_search
+		MusicPlatform.bd -> this@Logic::bd_search
+		MusicPlatform.kg -> this@Logic::kg_search
+	}(key, pageNum, cb, isAsync)
+
+
+	fun Music.findItem(cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
 
 	}
 
-	inline fun Music.findItem(cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+	fun MusicItem.download(file: File, cb: (Int?) -> Unit, isAsync: Boolean = true) {
 
 	}
 
-	inline fun MusicItem.download(file: File, cb: (Int?) -> Unit, isAsync: Boolean = true) {
+	// 网易云音乐搜索
+	fun wy_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
 
 	}
 
+	// 虾米音乐搜索
+	fun xm_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
+
+	}
+
+	// qq音乐搜索
+	fun qq_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
+
+	}
+
+	// 百度音乐搜索
+	fun bd_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
+
+	}
+
+	// 酷狗音乐搜索
+	fun kg_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
+	}
 }
