eb526346472171d4587b6c40de94883411c5ba6e
jiangKlijna <leil729564090@outlook.com>
2018-06-12 18:46:39 +0800

    :fire:update

--- src/main/java/com/jiangklijna/md/common/Logic.kt
+++ src/main/java/com/jiangklijna/md/common/Logic.kt
@@ -8,6 +8,7 @@ import java.io.File
 object Logic {
 
 
+	// 在一个平台下搜索音乐
 	fun MusicPlatform.search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true)
 			= when (this) {
 		MusicPlatform.wy -> this@Logic::wy_search
@@ -18,35 +19,68 @@ object Logic {
 	}(key, pageNum, cb, isAsync)
 
 
-	fun Music.findItem(cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+	// 搜索一个音乐的所有可下载选项
+	fun Music.findItem(cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true)
+			= when (this.platform) {
+		MusicPlatform.wy -> this@Logic::wy_findItem
+		MusicPlatform.xm -> this@Logic::xm_findItem
+		MusicPlatform.qq -> this@Logic::qq_findItem
+		MusicPlatform.bd -> this@Logic::bd_findItem
+		MusicPlatform.kg -> this@Logic::kg_findItem
+	}(this, cb, isAsync)
 
-	}
-
+	// 下载一个MusicItem
 	fun MusicItem.download(file: File, cb: (Int?) -> Unit, isAsync: Boolean = true) {
-
+		cb(null)
 	}
 
 	// 网易云音乐搜索
 	fun wy_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
-
+		cb(null)
 	}
 
 	// 虾米音乐搜索
 	fun xm_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
-
+		cb(null)
 	}
 
 	// qq音乐搜索
 	fun qq_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
-
+		cb(null)
 	}
 
 	// 百度音乐搜索
 	fun bd_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
-
+		cb(null)
 	}
 
 	// 酷狗音乐搜索
 	fun kg_search(key: String, pageNum: Int, cb: (List<Music>?) -> Unit, isAsync: Boolean = true) {
+		cb(null)
+	}
+
+	// 网易云音乐搜索
+	fun wy_findItem(music: Music, cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+		cb(null)
+	}
+
+	// 虾米音乐搜索
+	fun xm_findItem(music: Music, cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+		cb(null)
+	}
+
+	// qq音乐搜索
+	fun qq_findItem(music: Music, cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+		cb(null)
+	}
+
+	// 百度音乐搜索
+	fun bd_findItem(music: Music, cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+		cb(null)
+	}
+
+	// 酷狗音乐搜索
+	fun kg_findItem(music: Music, cb: (List<MusicItem>?) -> Unit, isAsync: Boolean = true) {
+		cb(null)
 	}
 }
