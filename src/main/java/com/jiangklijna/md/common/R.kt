81be55fd5321fb7a12c784b7f9a569018ea68698
jiangKlijna <leil729564090@outlook.com>
2018-06-26 19:39:36 +0800

    :fire:add R

---
+++ src/main/java/com/jiangklijna/md/common/R.kt
@@ -0,0 +1,29 @@
+package com.jiangklijna.md.common
+
+object R {
+    object url {
+        /**
+         * @s
+         */
+        const val WY_SEARCH = "http://music.163.com/#/search/m/?s="
+        /**
+         * @key
+         * resetful page
+         */
+        const val XM_SEARCH = "http://www.xiami.com/search/song/page/"
+        /**
+         * @w
+         * @page
+         */
+        const val QQ_SEARCH = "https://y.qq.com/portal/search.html#t=song"
+        /**
+         * @key
+         * @start = (page*20)
+         */
+        const val BD_SEARCH = "http://music.taihe.com/search/song"
+        /**
+         * @searchKeyWord
+         */
+        const val KG_SEARCH = "http://www.kugou.com/yy/html/search.html#searchType=song"
+    }
+}
