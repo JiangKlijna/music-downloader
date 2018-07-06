83490de1de10013177ea3dd25926f8c0760d682d
jiangKlijna <leil729564090@outlook.com>
2018-07-06 17:59:37 +0800

    :fire:dom4j to jsoup

--- src/main/java/com/jiangklijna/md/common/Resolve.kt
+++ src/main/java/com/jiangklijna/md/common/Resolve.kt
@@ -2,41 +2,44 @@ package com.jiangklijna.md.common
 
 import com.jiangklijna.md.bean.Music
 import okhttp3.Response
-import org.dom4j.io.SAXReader
+import org.jsoup.Jsoup
+import com.jiangklijna.md.common.Function.println
 
 // service
 object Resolve {
 
 	fun wy_search(response: Response?): List<Music> {
 		val doc = response.getDocument()
-
+		doc.title().println()
 		return emptyList()
 	}
 
 	fun xm_search(response: Response?): List<Music> {
 		val doc = response.getDocument()
-
+		doc.title().println()
 		return emptyList()
 	}
 
 	fun qq_search(response: Response?): List<Music> {
 		val doc = response.getDocument()
-
+		doc.title().println()
 		return emptyList()
 	}
 
 	fun bd_search(response: Response?): List<Music> {
 		val doc = response.getDocument()
-
+		doc.title().println()
 		return emptyList()
 	}
 
 	fun kg_search(response: Response?): List<Music> {
 		val doc = response.getDocument()
-
+		doc.title().println()
 		return emptyList()
 	}
 
 
-	fun Response?.getDocument() = SAXReader().read(this?.body()?.byteStream())
+//	fun Response?.getDocument() = SAXReader().read(this?.body()?.byteStream())
+
+	fun Response?.getDocument() = Jsoup.parse(this?.body()?.byteStream(), "utf-8", this?.request()?.url()?.toString())
 }
